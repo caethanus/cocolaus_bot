661639eb8152b18a8b9bc61a2b47b3d3e7f9c395
Caetano Machado <devcaethas@outlook.com>
2026-08-21 17:18:53 -0300

    esse vai ser o começo

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,16 @@
+import 'package:nyxx/nyxx.dart';
 
+void main() async {
+  final client = await Nyxx.connectGateway('<TOKEN>', GatewayIntents.allUnprivileged);
 
-void main() {
+  final botUser = await client.users.fetchCurrentUser();
 
-
-
+  client.onMessageCreate.listen((event) async {
+    if (event.mentions.contains(botUser)) {
+      await event.message.channel.sendMessage(MessageBuilder(
+        content: 'You mentioned me!',
+        referencedMessage: MessageReferenceBuilder.reply(messageId: event.message.id),
+      ));
+    }
+  });
 }
