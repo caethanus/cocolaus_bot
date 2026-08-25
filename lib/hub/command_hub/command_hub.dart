9013a712ba5ce2a6c0e66b14ff55a28043768c1d
Caetano Machado <devcaethas@outlook.com>
2026-08-25 14:14:58 -0300

    Update command_hub.dart

--- lib/hub/command_hub/command_hub.dart
+++ lib/hub/command_hub/command_hub.dart
@@ -23,6 +23,8 @@ class CommandHub {
   ChatCommand registerUser = ChatCommand('registrar', 'Cadastra no banco do bot um dos guys para trazer a coquinha da semana.', (ChatContext context, @Description('Nome do usuário') String username, [@Description('Usuário cadastrado') User? discordUser]) async {
     final idDiscord = discordUser?.id.toString() ?? context.user.id.toString();
 
+    //TODO Caetano: aqui vai ter que ser implemntado o método correspondente para cadastrar no banco do bot
+
     await context.respond(MessageBuilder(content: '${'$username $idDiscord'} Cadastrado com sucesso!'));
   });
 }
