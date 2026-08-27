0897cb913ec2dbe8c0d5947666f39eacc0e5deeb
Caetano Machado <devcaethas@outlook.com>
2026-08-27 11:05:02 -0300

    diminuir escopo do PR

--- lib/modules/dia_coca/repository/dia_coca_repository_interface.dart
+++
@@ -1,4 +0,0 @@
-import 'package:cocolaus_bot/modules/dia_coca/entity/dia_coca.dart';
-import 'package:cocolaus_bot/shared/repository/base_repository_interface.dart';
-
-abstract class IDiaCocaRepository extends IBaseRepository<DiaCocaEntity> {}
