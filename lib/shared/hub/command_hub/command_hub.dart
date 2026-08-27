4a0712e79780015315dbf045ecbaf4d40d34b1da
Caetano Machado <devcaethas@outlook.com>
2026-08-27 11:18:09 -0300

    comentando comandos que não vão funcionar por agora

--- lib/shared/hub/command_hub/command_hub.dart
+++ lib/shared/hub/command_hub/command_hub.dart
@@ -22,27 +22,25 @@ class CommandHub {
   }
 
   ///Lista de comandos, declarados em variáveis para passar na lista de commands e registrar na instância de criação do bot
-  ChatCommand presentation = ChatCommand('apresentacao', 'Comando para testes iniciais do bot no discord', (ChatContext context) async {
-    await context.respond(MessageBuilder(content: 'Salve tropa!'));
-  });
+  //todo: Reaver esse comando ChatCommand presentation = ChatCommand('apresentacao', 'Comando para testes iniciais do bot no discord', (ChatContext context) async {await context.respond(MessageBuilder(content: 'Salve tropa!'));});
 
-  ChatCommand takeIt = ChatCommand('tome', 'Mostra uma mensagem especial para um determinado usuario, ou para todos.', (ChatContext context, [@Description('Quem') String? user]) async {
-    String mensagem;
-    if (user != null) {
-      mensagem = '$user pois tomeeeee';
-    } else {
-      mensagem = 'pois tomeeeee';
-    }
+  //todo: Reaver esse comando  ChatCommand takeIt = ChatCommand('tome', 'Mostra uma mensagem especial para um determinado usuario, ou para todos.', (ChatContext context, [@Description('Quem') String? user]) async {
+  //   String mensagem;
+  //   if (user != null) {
+  //     mensagem = '$user pois tomeeeee';
+  //   } else {
+  //     mensagem = 'pois tomeeeee';
+  //   }
+  //
+  //   await context.respond(MessageBuilder(content: mensagem));
+  // });
 
-    await context.respond(MessageBuilder(content: mensagem));
-  });
-
-  ChatCommand coffeTime = ChatCommand('coffe-time', 'Chama todos para tomarem café.', (ChatContext context) async {
-    if (context.channel.id.toString() != EnumCanaisExclusivos.coffe.id) {
-      await context.respond(MessageBuilder(content: 'Esse comando só pode ser usado no canal #coffe-time'));
-
-      return;
-    }
-    await context.respond(MessageBuilder(content: '@here coffe time!!!'));
-  });
+  //todo: Reaver esse comando ChatCommand coffeTime = ChatCommand('coffe-time', 'Chama todos para tomarem café.', (ChatContext context) async {
+  //   if (context.channel.id.toString() != EnumCanaisExclusivos.coffe.id) {
+  //     await context.respond(MessageBuilder(content: 'Esse comando só pode ser usado no canal #coffe-time'));
+  //
+  //     return;
+  //   }
+  //   await context.respond(MessageBuilder(content: '@here coffe time!!!'));
+  // });
 }
