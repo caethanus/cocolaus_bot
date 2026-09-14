0fa8e5863e7e947280d784dd1754bea8300c1e68
Caetano Machado <devcaethas@outlook.com>
2026-09-14 20:15:34 -0300

    Update base_repository.dart

--- lib/shared/repository/base_repository.dart
+++ lib/shared/repository/base_repository.dart
@@ -26,9 +26,7 @@ abstract class BaseRepository<E extends IBaseEntity> implements IBaseRepository<
     entity.base.criadoEm ??= DateTime.now();
 
     final values = toMap(entity);
-
-    print(values);
-
+    
     final columns = values.keys.join(', ');
     final placeholders = List.filled(values.length, '?').join(', ');
 
